0a5ce6c540e04ca7efe1784822bb0aea8ac3a82c
Hanlon Miller <hanlonmiller@yahoo.com>
2019-04-20 17:18:13 -0400

    Initial Commit

---
+++ HTTP Requestor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HTTP Requestor
+//
+//  Created by Hanlon Miller on 4/20/19.
+//  Copyright © 2019 Hanlon Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HTTP RequestorTests/HTTP_RequestorTests.swift
@@ -0,0 +1,34 @@
+//
+//  HTTP_RequestorTests.swift
+//  HTTP RequestorTests
+//
+//  Created by Hanlon Miller on 4/20/19.
+//  Copyright © 2019 Hanlon Miller. All rights reserved.
+//
+
+import XCTest
+@testable import HTTP_Requestor
+
+class HTTP_RequestorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
